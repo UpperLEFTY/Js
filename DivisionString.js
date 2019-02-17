b87e9e513e03bfb71c3f8995d6bace414fc44f44
Essence Alexander <upperlefty@zoho.eu>
2019-02-16 22:30:39 -0800

    Update DivisionString.js

--- DivisionString.js
+++ DivisionString.js
@@ -3,7 +3,11 @@ function DivisionStringified(num1,num2) {
   var total = Math.round(num1/num2)
   var arr = total.toString().split("");
   
+  
   var number = arr.length%3;
   
-   for (var i = 3; i > number; i--){
+   
+  for (var i = 3; i > number; i--){
   arr.unshift("X");
+     
+     }
